2311f874f066de1a3701968bb9a29f6257a178d2
Max Altena <Max@MaxAltena.com>
2018-02-15 23:53:28 +0100

    category updating

--- categorie.php
+++ categorie.php
@@ -17,6 +17,9 @@ if (isset($_GET['c'])) {
             $short = $_GET['c'];
             $page = $short;
             
+            $item = new Item;
+            $items = $item->fetch_for_preview_by_category($short);
+            
             // Get items
 ?>
 <html lang="nl">
@@ -27,13 +30,53 @@ if (isset($_GET['c'])) {
     </head>
 
     <body>
-        <?php include('includes/menu.php'); ?>
         <?php include_once('includes/loader.php'); ?>
+        <?php include_once('includes/menu.php'); ?>
         <main>
             <div id="top">
+                <!-- Top = short name, full name and description? -->
                 <h1><?= $short; ?></h1>
             </div>
+            <div id="center">
+                <!-- Filter per sprint -->
+                <div style="background: yellow;">
+                    Sprint1
+                </div>
+                <?php
+                    foreach ($items as $item) {
+                        // Switch to see what sprint item is from
+                        // If sprint item counter is not 0, create sprint header
+                        // Else do not create sprint header
+                        // 
+                        
+                        
+                        echo('<span class="divider"></span><a href="https://i371527.hera.fhict.nl/item?i='.$item['id'].'" class="itemLink" id="itemLink'.$item['id'].'"><div class="section"><div class="firstClass"><h1>'.$item['name'].'</h1><h2>'.$item['description'].'</h2></div><div><span class="arrowSpan"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="arrow"><path class="arrowPath" d="M24 11.871l-5-4.871v3h-19v4h19v3z"/></svg></span></div></div></a>');
+                        
+                        $photoID = strstr($item['photos'], '|', TRUE);
+                        $photo = new Photo;
+                        $photoInsert = $photo->fetch_by_id($photoID);
+
+                        if ($photoInsert !== null) {
+                            echo('<script>$("#itemLink'.$item['id'].'").css({background: "url(/assets/photos/'.$photoInsert.')"})</script>');
+                        }
+                    }
+                ?>
+                <!-- Load in items - get name, get description? -->
+                <span class="divider"></span>
+            </div>
+            <div id="bottom">
+                <!-- Back to homepage and back to top button? -->
+                <span id="backtoHome" class="backtoHome">
+                    <span class="footerLink">Terug naar homepagina</span>
+                </span>
+                <span id="backLine"></span>
+                <span id="backtoTop" class="backtoTop">
+                    <span class="footerLink">Terug naar boven</span>
+                </span>
+            </div>
         </main>
+        <script src="js/category.js"></script>
+        <script src="js/menu.js"></script>
         <?php include_once('includes/menuselect.php'); ?>
     </body>
 </html>
--- includes/query.php
+++ includes/query.php
@@ -9,6 +9,21 @@ class Category {
     }
 }
 
+class Item {
+    public function fetch_for_preview_by_category($short) {
+        global $PDO;
+        $getIDfromSHORT = $PDO->prepare('SELECT id FROM categories WHERE short = ?');
+        $getIDfromSHORT->bindValue(1, $short);
+        $getIDfromSHORT->execute();
+        $categoryID = $getIDfromSHORT->fetch(PDO::FETCH_COLUMN, 0);
+        
+        $getITEMSfromCAT = $PDO->prepare('SELECT id, name, description, photos, sprint FROM items WHERE category = ? ORDER BY sprint ASC');
+        $getITEMSfromCAT->bindValue(1, $categoryID);
+        $getITEMSfromCAT->execute();
+        return $getITEMSfromCAT->fetchAll();
+    }
+}
+
 class Photo {
     public function fetch_by_id($id) {
         global $PDO;
